bc8970dc4f78216810c02482c11a2522dd04919f
Chris <chrisdominguez2001@gmail.com>
2022-07-22 16:14:41 -0400

    updated

--- Tutor/accounts/models.py
+++ Tutor/accounts/models.py
@@ -2,6 +2,8 @@ from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,8 +13,20 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     country = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
-    bio = models.CharField(max_length=250)
+    bio = models.TextField(blank = True)
+    
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)   #getting image
+
+        #resize image if grater than 300px
+        if img.height > 300 or img.width >300:
+            im_size = (300, 300)
+            img.thumbnail(im_size)
+            img.save(self.image.path)
     
